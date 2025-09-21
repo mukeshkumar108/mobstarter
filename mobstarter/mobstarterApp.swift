858386f80c789c89d6f015bcfc9adbb1f4e9b145
kaiser <kaiser.kumar108@gmail.com>
2025-09-21 14:45:45 +0100

    Initial Commit

---
+++ mobstarter/mobstarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobstarterApp.swift
+//  mobstarter
+//
+//  Created by Mukesh Kumar on 21/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct mobstarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
